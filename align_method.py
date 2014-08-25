dd5237c2889c5b6eecc6928ab1f7e0d33e39fbd3
licode <lili@bnl.gov>
2014-08-25 11:45:57 -0400

    add doc on align_calculator

--- align_method.py
+++ align_method.py
@@ -21,7 +21,6 @@ def alignment_on_edge(data, padv=14, maxv=True):
         how much is shift for each horizontal position
     array 2D :
         aligned data
-
     """
     d_shape = data.shape
 
@@ -42,201 +41,223 @@ def alignment_on_edge(data, padv=14, maxv=True):
     # do correction
     for i in range(datas[1]):
         diff = mlist[i] - mlist[0]
-        #print padv+diff, -padv+diff
-        #print mlist[i], diff
         data0[padv:-padv, i] = data0[padv+diff:-padv+diff, i]
 
     return mlist, data0[padv:-padv, :]
 
 
-    def calculate_alignment_derivative(self, padv = 10, maxv=True):
-        """
-        do alignment based on max or min value of derivative
-        """
-        data = self.data
-        datas = data.shape 
-            
-        data0 = np.zeros([datas[0]+padv*2, datas[1]])
-        data0[padv:-padv, :] = data
-        
-        mlist = []
-        
-        # get shift value
-        for i in range(datas[1]):
-            grad = data[1:, i] - data[0:-1, i]
-            if maxv == True:
-                pos = np.where(grad == np.max(grad))
-            else:
-                pos = np.where(grad == np.min(grad))
-            mlist.append(pos[0])
-      
-        datan = np.zeros([datas[0]+padv*2, datas[1]])
-        
-        # do correction
-        for i in range(datas[1]):
-            diff = mlist[i]-mlist[0]
-            print padv+diff, -padv+diff
-            datan[padv:-padv,i] = data0[padv+diff:-padv+diff, i]
-        
-        datan = datan[padv:-padv, :]
-        
-        data = datan
-        
-        return mlist, data
+def alignment_on_derivative(data, padv=10, maxv=True):
+    """
+    Do vertical alignment based on max value or min of derivative.
+
+    Parameters
+    ----------
+    data : 2D array
+        input data for alignment
+    padv : int
+        add padding value on vertical direction
+    maxv : bool
+        choose max value or min value
+
+    Returns:
+    --------
+    list :
+        how much is shift for each horizontal position
+    array 2D :
+        aligned data
+    """
+    d_shape = data.shape
+
+    data0 = np.zeros([d_shape[0]+padv*2, d_shape[1]])
+    data0[padv:-padv, :] = data
+
+    mlist = []
+
+    # get shift value
+    for i in range(d_shape[1]):
+        grad = data[1:, i] - data[0:-1, i]
+        if maxv is True:
+            pos = np.where(grad == np.max(grad))
+        else:
+            pos = np.where(grad == np.min(grad))
+        mlist.append(pos[0])
+
+    #datan = np.zeros([datas[0]+padv*2, datas[1]])
+
+    # do correction
+    for i in range(datas[1]):
+        diff = mlist[i] - mlist[0]
+        #print padv+diff, -padv+diff
+        data0[padv:-padv, i] = data0[padv+diff:-padv+diff, i]
+
+    return mlist, data0[padv:-padv, :]
 
 
+def alignment_on_correlation(data, padv=2):
+    """
+    Do vertical alignment based on cross correlation.
 
-    def calculate_alignment_corr(self, padv = 2):
-        """
-        alignment based on cross correlation
-        bin the data to high dim for sub-pixel alignment
-        """
-        
-        data = self.data
-        datas = data.shape    
-            
-        #data0 = np.zeros([datas[0]+padv*2,datas[1]])
-        #data0[padv:-padv,:] = data
-        data0 = data
-        
-        mlist = []
-        cor_all = []
-        
-        # get shift value
-        for i in range(datas[1]):
-            cor_list = []
-            data1 = data0[padv:-padv,0]
-            # calculate cross correlation
-            for j in range(-padv,padv):
-                data2 = data0[padv+j:-padv+j,i]
-                
-                cor = get_correlation(data1, data2)
-                cor_list.append(cor)
-                
-            cor_list = np.array(cor_list)
-            pos = np.where(cor_list==np.max(cor_list))
-            mlist.append(pos[0])
-            cor_all.append(cor_list)
-            
-        #datan = np.zeros([datas[0]+padv*2,datas[1]])
-        datan = np.zeros([datas[0],datas[1]])
-        
-        # do correction
-        for i in range(datas[1]):
-            diff = mlist[i]-mlist[0]
-            print diff
-            datan[padv:-padv,i] = data0[padv+diff:-padv+diff, i]
-        
-        datan = datan[padv:-padv,:]
-        
-        return mlist-mlist[0], datan, cor_all
+    Parameters
+    ----------
+    data : 2D array
+        input data for alignment
+    padv : int
+        add padding value on vertical direction
 
+    Returns:
+    --------
+    list :
+        shift position for each horizontal position
+    array 2D :
+        aligned data
+    cor_all : 2D array
+        correlation value for each horizontal position
+    """
 
-    def calculate_alignment_corr_bin(self, padv = 2, bin_n = 1):
-        """
-        alignment based on cross correlation
-        bin the data to high dim for sub-pixel alignment
-        """
-        
-        data = self.data
-        datas = data.shape
-        
-        # bin the data
-        databin = np.zeros([datas[0]*bin_n,datas[1]])
-        databin_s = databin.shape
-        
-        for i in range(datas[1]):
-            data_temp = rebin_data_expand(data[:,i], m=bin_n)
-            # linear interpolation
-            #data_temp = np.histogram(data[:,i],bins=databin_s[0])
-            databin[:,i] = data_temp#[1][0:-1]
-    
-            
-        data = databin
-        datas = databin_s
-    
-            
-        #data0 = np.zeros([datas[0]+padv*2,datas[1]])
-        #data0[padv:-padv,:] = data
-        data0 = data
-        
-        mlist = []
-        
-        # get shift value
-        for i in range(datas[1]):
-            cor_list = []
-            data1 = data0[padv:-padv,0]
-            # calculate cross correlation
-            for j in range(-padv,padv):
-                data2 = data0[padv+j:-padv+j,i]
-                
-                cor = get_correlation(data1, data2)
-                cor_list.append(cor)
-                
-            cor_list = np.array(cor_list)
-            pos = np.where(cor_list==np.max(cor_list))
-            mlist.append(pos[0])
-        
-        #datan = np.zeros([datas[0]+padv*2,datas[1]])
-        datan = np.zeros([datas[0],datas[1]])
-        
-        # do correction
-        for i in range(datas[1]):
-            diff = mlist[i]-mlist[0]
-            print diff
-            datan[padv:-padv,i] = data0[padv+diff:-padv+diff, i]
-        
-        datan = datan[padv:-padv,:]
-        
-        
-        data = datan
-        
-        
-        # rebin it back
-        datanobin = np.zeros([datas[0]/bin_n,datas[1]])
-        datanobin_s = datanobin.shape
-        
-        for i in range(datanobin_s[1]):
-            data_temp = rebin_data_shrink(data[:,i], m=bin_n)
-            #data_temp = np.histogram(data[:,i],bins=datanobin_s[0])
-            datanobin[:,i] = data_temp#[1][0:-1]
-        
-        return mlist-mlist[0], datanobin
+    d_shape = data.shape
+
+    mlist = []
+    cor_all = []
+
+    # get shift value
+    data1 = data[padv:-padv, 0]
+
+    for i in range(d_shape[1]):
+        cor_list = []
+
+        # calculate cross correlation
+        for j in range(-padv, padv):
+            data2 = data[padv+j:-padv+j, i]
+            cor = get_correlation(data1, data2)
+            cor_list.append(cor)
+
+        cor_list = np.array(cor_list)
+        pos = np.where(cor_list == np.max(cor_list))
+        mlist.append(pos[0])
+        cor_all.append(cor_list)
+
+    # do correction
+    diff_list = np.array(mlist) - mlist[0]
+    for i in range(d_shape[1]):
+        data[padv:-padv, i] = data[padv+diff_list[i]:-padv+diff_list[i], i]
+
+    return diff_list, data[padv:-padv, :], cor_all
 
 
-    def do_vertical_alignment(self, center_list, data_all):
-        """
-        adjust center position of y according to center_list
-        """
+def calculate_alignment_corr_bin(data, padv=2, bin_n=1):
+    """
+    alignment based on cross correlation
+    bin the data to high dim for sub-pixel alignment
+    """
 
-        s = data_all.shape
-        
-        padv = np.max(np.abs(center_list))
-        
-        data_aligny = np.zeros([s[0], s[1], s[2]])
-        
-        for i in range(s[0]):
-            d = data_all[i,:,:]
-            d = np.reshape(d,[s[1],s[2]])
-            
-            cenv = center_list[i,0]
-            data_temp = np.zeros([s[1]+2*padv, s[2]])
-            data_temp[padv:-padv,:] = d
-            data_aligny[i,:,:] = data_temp[padv+cenv:-padv+cenv,:]
-            
-            
-        return data_aligny    
+    datas = data.shape
+
+    # bin the data
+    databin = np.zeros([datas[0]*bin_n, datas[1]])
+    databin_s = databin.shape
+
+    for i in range(datas[1]):
+        data_temp = rebin_data_expand(data[:, i], m=bin_n)
+        # linear interpolation
+        #data_temp = np.histogram(data[:,i],bins=databin_s[0])
+        databin[:, i] = data_temp#[1][0:-1]
+
+
+    data = databin
+    datas = databin_s
+
+
+    #data0 = np.zeros([datas[0]+padv*2,datas[1]])
+    #data0[padv:-padv,:] = data
+    data0 = data
+
+    mlist = []
+
+    # get shift value
+    for i in range(datas[1]):
+        cor_list = []
+        data1 = data0[padv:-padv, 0]
+        # calculate cross correlation
+        for j in range(-padv,padv):
+            data2 = data0[padv+j:-padv+j, i]
+
+            cor = get_correlation(data1, data2)
+            cor_list.append(cor)
+
+        cor_list = np.array(cor_list)
+        pos = np.where(cor_list == np.max(cor_list))
+        mlist.append(pos[0])
+
+    #datan = np.zeros([datas[0]+padv*2,datas[1]])
+    datan = np.zeros([datas[0],datas[1]])
+
+    # do correction
+    for i in range(datas[1]):
+        diff = mlist[i]-mlist[0]
+        print diff
+        datan[padv:-padv, i] = data0[padv+diff:-padv+diff, i]
+
+    datan = datan[padv:-padv, :]
+
+
+    data = datan
+
+
+    # rebin it back
+    datanobin = np.zeros([datas[0]/bin_n,datas[1]])
+    datanobin_s = datanobin.shape
+
+    for i in range(datanobin_s[1]):
+        data_temp = rebin_data_shrink(data[:,i], m=bin_n)
+        #data_temp = np.histogram(data[:,i],bins=datanobin_s[0])
+        datanobin[:,i] = data_temp#[1][0:-1]
+
+    return mlist-mlist[0], datanobin
+
+
+def do_vertical_alignment(center_list, data_all):
+    """
+    adjust center position of y according to center_list
+    """
+
+    s = data_all.shape
+
+    padv = np.max(np.abs(center_list))
+
+    data_aligny = np.zeros([s[0], s[1], s[2]])
+
+    for i in range(s[0]):
+        d = data_all[i,:,:]
+        d = np.reshape(d,[s[1],s[2]])
+
+        cenv = center_list[i,0]
+        data_temp = np.zeros([s[1]+2*padv, s[2]])
+        data_temp[padv:-padv,:] = d
+        data_aligny[i,:,:] = data_temp[padv+cenv:-padv+cenv,:]
+
+
+    return data_aligny
     
 
 
 def get_correlation(data1, data2):
     """
-    calculate cross correlation between two data sets
+    Calculate cross correlation between two data sets.
+    Parameters
+    ----------
+    data1 : 1D array
+        data set 1
+    data2 : 1D array
+        data set 2
+
+    Returns
+    -------
+    float :
+        correlation between the two
     """
     data1 = np.array(data1)
     data2 = np.array(data2)
-    #print data1.shape, data2.shape
-    return np.dot(data1,data2)/np.sqrt(np.dot(data1,data1))/np.sqrt(np.dot(data2,data2))
+    return np.dot(data1, data2)/np.sqrt(np.dot(data1, data1))/np.sqrt(np.dot(data2, data2))
 
 
 def get_correlation2D(data1, data2):
@@ -337,11 +358,3 @@ def rm_wrap(data):
 
       
     return data
-
-    
-    
-    
-    
-    
-
-
